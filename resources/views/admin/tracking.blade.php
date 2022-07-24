7e4ae3337150a1a9718e3c8b1579ceb3707078d3
wisnubaldas <wisnubaldas@gmail.com>
2022-07-25 00:40:12 +0700

    template excel

--- resources/views/admin/tracking.blade.php
+++ resources/views/admin/tracking.blade.php
@@ -8,7 +8,11 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0 font-size-18">Welcome Back! {{auth()->user()->name}}</h4>
+                            <h4 class="mb-sm-0 font-size-18">
+                                <a href="/tracking_sample.xlsx">
+                                Download Sample Upload Excel !
+                                </a>
+                            </h4>
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
